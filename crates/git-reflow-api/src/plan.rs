9557bb0065d4ca4d5554ff9d3a1b9c880991d70a
Jonathan Hiles <jonathan@hil.es>
2026-09-21 20:46:48 +1000

    feat(api): allow selecting packages to plan releases for

--- crates/git-reflow-api/src/plan.rs
+++ crates/git-reflow-api/src/plan.rs
@@ -1,6 +1,5 @@
 use crate::git_cliff::{get_version_from_git_cliff_context, render_changelog_markdown, run_git_cliff};
 use crate::settings::AppConfig;
-use crate::settings::pkg::PackageConfig;
 use anyhow::Context;
 use handlebars::Handlebars;
 use semver::Version;
@@ -8,10 +7,11 @@ use serde_json::{Value, json};
 use tracing::debug;
 
 /// A planned package release.
-#[allow(unused)]
-pub struct PackageRelease<'pkg> {
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct PackageRelease {
     /// The package configuration.
-    pub pkg: &'pkg PackageConfig,
+    pub package_name: String,
     /// The current version of the package, if it exists.
     pub current_version: Option<Version>,
     /// The next (target) version of the package.
@@ -29,27 +29,37 @@ pub struct PackageRelease<'pkg> {
 /// # Arguments
 ///
 /// * `config` - The app configuration.
+/// * `package_names` - The package names to plan releases for (leave empty for all).
 /// * `target_branch` - The target branch for the release.
 ///
 /// # Returns
 ///
 /// A result of all planned package release information.
-pub async fn plan_releases<'pkg>(
-    config: &'pkg AppConfig,
+pub async fn plan_releases(
+    config: &AppConfig,
+    package_names: &[String],
     target_branch: &str,
-) -> anyhow::Result<Vec<PackageRelease<'pkg>>> {
+) -> anyhow::Result<Vec<PackageRelease>> {
     let mut planned_releases = Vec::with_capacity(config.packages.len());
     for pkg in &config.packages {
+        // Skip packages that are not in the list of package names to plan releases for, if any.
+        let pkg_name = pkg.name().to_string();
+        if !package_names.is_empty() && !package_names.contains(&pkg_name) {
+            debug!("skipping {}", pkg_name);
+            continue;
+        }
+
         // Run `git-cliff` in the package directory.
         let context = match run_git_cliff(&pkg.dir, None)? {
             // If the context contains a bump type, it indicates that there are changes to be released.
-            Some(ctx) if !ctx["bump_type"].is_null() => {
-                debug!("{} has unreleased changes ✏️", pkg.name());
+            // If the context does not contain a previous version, it also indicates that this is the first release.
+            Some(ctx) if !ctx["bump_type"].is_null() || ctx["previous"].is_null() => {
+                debug!("{} has unreleased changes ✏️", pkg_name);
                 ctx
             }
             // Otherwise, there are no changes to be released.
             _ => {
-                debug!("{} is up-to-date ✅", pkg.name());
+                debug!("{} is up-to-date ✅", pkg_name);
                 continue;
             }
         };
@@ -64,7 +74,7 @@ pub async fn plan_releases<'pkg>(
             .render_template(
                 &config.git.commit_message_pattern,
                 &json!({
-                    "package": pkg.name(),
+                    "package": pkg_name,
                     "scope": pkg.scope(),
                     "version": next_version.to_string(),
                     "branch": target_branch,
@@ -74,7 +84,7 @@ pub async fn plan_releases<'pkg>(
 
         // Append the planned release information for this package.
         planned_releases.push(PackageRelease {
-            pkg,
+            package_name: pkg_name,
             current_version,
             next_version,
             commit_message,
@@ -94,7 +104,6 @@ mod tests {
     use rstest::rstest;
     use std::fs;
     use std::io::Write;
-    use std::path::Path;
 
     /// Tests that package releases are planned correctly.
     #[rstest]
@@ -129,10 +138,10 @@ mod tests {
 
         // Call `plan_releases` and assert that the planned releases are as expected.
         // NB: We expect two planned releases: one for the root package and one for the `example-api` package.
-        let planned = plan_releases(&config, "main").await.unwrap();
+        let planned = plan_releases(&config, &[], "main").await.unwrap();
         assert_eq!(planned.len(), 2);
-        assert_eq!(planned[0].pkg.dir, Path::new("."));
-        assert_eq!(planned[1].pkg.dir, Path::new("crates/example-api"));
+        assert_eq!(planned[0].package_name, "example-rust-workspace");
+        assert_eq!(planned[1].package_name, "example-api");
         assert_eq!(planned[0].current_version, Some(Version::parse("1.0.0").unwrap()));
         assert_eq!(planned[1].current_version, Some(Version::parse("1.0.0").unwrap()));
         assert_eq!(planned[0].next_version, Version::parse("1.1.0").unwrap());
@@ -143,5 +152,45 @@ mod tests {
         assert!(!planned[1].changelog_md.is_empty());
         assert!(planned[0].context.is_object());
         assert!(planned[1].context.is_object());
+    }
+
+    /// Tests that only the selected package releases are planned correctly.
+    #[rstest]
+    #[tokio::test]
+    async fn test_plan_releases_with_selected(#[with("example-rust-workspace")] project_repo: (TempDir, Repository)) {
+        let (project_dir, repo) = project_repo;
+
+        // Load the configuration for the project repository.
+        std::env::set_current_dir(&project_dir).unwrap();
+        let config = crate::settings::load(None).unwrap();
+
+        // Commit the current files to the `main` branch.
+        repo.set_head("refs/heads/main").unwrap();
+        crate::git::commit(&repo, &["."], "chore: initial commit").unwrap();
+
+        // Create Git tags for the current versions of the packages.
+        let commit = repo.head().unwrap().peel_to_commit().unwrap();
+        repo.tag_lightweight("v1.0.0", commit.as_object(), false).unwrap();
+        repo.tag_lightweight("example-api-v1.0.0", commit.as_object(), false)
+            .unwrap();
+        repo.tag_lightweight("example-cli-v1.0.0", commit.as_object(), false)
+            .unwrap();
+
+        // Create some changes in the packages and commit those changes.
+        let lib_rs_path = project_dir.path().join("crates/example-api/src/lib.rs");
+        writeln!(
+            fs::OpenOptions::new().append(true).open(&lib_rs_path).unwrap(),
+            "\npub fn subtract(a: i32, b: i32) -> i32 {{ a - b }}"
+        )
+        .unwrap();
+        crate::git::commit(&repo, &["."], "feat(api): add a `subtract` function").unwrap();
+
+        // Call `plan_releases` with a selection of package names and assert that the planned releases are as expected.
+        // NB: We expect one planned release: the `example-api` package as it is the only one selected.
+        let planned = plan_releases(&config, &["example-api".to_string()], "main")
+            .await
+            .unwrap();
+        assert_eq!(planned.len(), 1);
+        assert_eq!(planned[0].package_name, "example-api");
     }
 }
